e437bde5506ee17f267319fdeb4a3a8a2c97538a
AndreyPodch <number183@yandex.ru>
2020-10-06 13:53:36 +0300

    Add (maybe not working) scan DenseMatrix

--- src/main/java/edu/spbu/matrix/DenseMatrix.java
+++ src/main/java/edu/spbu/matrix/DenseMatrix.java
@@ -1,16 +1,40 @@
 package edu.spbu.matrix;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  * Плотная матрица
  */
 public class DenseMatrix implements Matrix
 {
+  ArrayList<Integer> data;
+  int w,h;
   /**
    * загружает матрицу из файла
    * @param fileName
    */
   public DenseMatrix(String fileName) {
-
+    Scanner datascan = new Scanner(fileName);
+    this.h=0;
+    this.data=new ArrayList<>();
+    while(datascan.hasNextLine())
+    {
+      this.h++;
+      String[] array = datascan.nextLine().split(" ");
+      this.w=array.length;
+      for(int i=0;i<array.length;i++)
+      {
+        int el = Integer.parseInt(array[i]);
+        if(!data.add(el))
+        {
+          System.out.println("Memory allocation error");
+        }
+      }
+    }
   }
   /**
    * однопоточное умнджение матриц
